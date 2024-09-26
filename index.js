12126081e0ad3ced4397653fe43e485da4636f70
Irene Mutegi <irene@Ms-Echo.>
2024-09-26 21:37:15 +0300

    short message

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your solution in this file!
+
+var customerName = 'bob';
+console.log (customerName);
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+upperCaseCustomerName();
+
+
+console.log(customerName);
+
+
+function setBestCustomer() { 
+    bestCustomer = 'not bob';
+}
+setBestCustomer();
+console.log(bestCustomer); 
+
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'; 
+}
+
+
+setBestCustomer();
+console.log(bestCustomer);
+
+overwriteBestCustomer();
+console.log(bestCustomer); 
+
+const leastFavoriteCustomer = 'some customer';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'another customer';
+}
+try {
+    changeLeastFavoriteCustomer();
+} catch (error) {
+    console.log(error.message); 
+}
+console.log(leastFavoriteCustomer);
